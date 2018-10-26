f988b33cc26ba5c93c069ef2eb72056219b7c544
Stephen Hart <stephen@cardswitcher.co.uk>
2018-10-26 16:05:55 +0100

    API's imported

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,14 @@
+const Shares = require ('./models/shares_portfolio.js');
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Javascript loaded');
 
 
+
+  const shares = new Shares();
+  shares.getQuoteData();
+  shares.getCompanyData();
+  shares.getSymbolData();
+  shares.getChartData();
 });
---
+++ client/src/models/shares_portfolio.js
@@ -0,0 +1,51 @@
+const PubSub = require('../helpers/pub_sub.js');
+const Request = require('../helpers/request.js')
+
+const Shares = function () {
+};
+
+  Shares.prototype.getQuoteData = function () {
+  const request = new Request("https://api.iextrading.com/1.0/stock/aapl/quote");
+  request.get().then((quote) => {
+    this.quote = quote
+  console.log(this.quote);
+    PubSub.publish('Share:share-data-ready', quote)
+  });
+};
+
+  Shares.prototype.getCompanyData = function () {
+  const request = new Request("https://api.iextrading.com/1.0/stock/aapl/company");
+  request.get().then((company) => {
+    this.company = company
+console.log(this.company);
+    PubSub.publish('Company:company-data-ready', company)
+  });
+};
+
+  Shares.prototype.getSymbolData = function () {
+  const request = new Request("https://api.iextrading.com/1.0/stock/aapl/chart/1y");
+  request.get().then((symbol) => {
+    this.symbol = symbol
+  console.log(this.symbol);
+    PubSub.publish('Company:company-data-ready', symbol)
+  });
+};
+
+Shares.prototype.getChartData = function () {
+const request = new Request("https://api.iextrading.com/1.0/ref-data/symbols");
+request.get().then((chart) => {
+  this.chart = chart
+console.log(this.chart);
+  PubSub.publish('Company:company-data-ready', chart)
+});
+};
+
+
+// Shares.prototype.extend = function (obj, src) {
+//   for (var key in src) {
+//       if (src.hasOwnProperty(key)) obj[key] = src[key];
+//   }
+//   return obj;
+
+
+module.exports = Shares;
