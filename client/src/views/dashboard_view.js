76f7c1f85804daeee0c8587ac9bf2e9f572607a7
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-10-30 11:24:32 +0000

    Implement dashboard button

--- client/src/views/dashboard_view.js
+++ client/src/views/dashboard_view.js
@@ -4,8 +4,17 @@ const DashboardView = function (element) {
 }
 
 DashboardView.prototype.bindEvents = function () {
+  this.renderDashboard();
+  PubSub.subscribe('NavView:dashboard-button-clicked', () => {
+    this.renderDashboard();
+  })
+};
+
+
+DashboardView.prototype.renderDashboard = function () {
+  this.element.innerHTML='';
   const test = document.createElement('h1');
-  test.textContent = 'test';
+  test.textContent = 'dashboard view';
   this.element.appendChild(test);
 };
 
--- client/src/views/nav_view.js
+++ client/src/views/nav_view.js
@@ -8,6 +8,10 @@ NavView.prototype.bindEvents = function () {
   const my_shares_button = document.querySelector('#my-shares-button');
   my_shares_button.addEventListener('click', () => {
     PubSub.publish('NavView:my-shares-button-clicked', my_shares_button.value);
+  });
+  const dashboard_button = document.querySelector('#dashboard-button');
+  dashboard_button.addEventListener('click', () => {
+    PubSub.publish('NavView:dashboard-button-clicked', dashboard_button.value);
   })
 };
 
